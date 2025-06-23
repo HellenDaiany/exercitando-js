7d55bc413c521effea351666e3b23b58b7cd6281
Hellen Daiany <hellendaiiany@gmail.com>
2025-06-23 19:15:43 -0300

    add interações

--- exercicio02/script.js
+++ exercicio02/script.js
@@ -1,1 +1,48 @@
+function verificar() {
+    var data = new Date ()
+    var ano =data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = document.querySelector('#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+        
+    }else{
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value) 
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+       img.width = 150;
+       img.height = 150;
+       img.style.borderRadius = "50%"
+       img.style.objectFit = "cover"
+       
+       if(fsex[0].checked){
+        genero = 'Homem'
+        if(idade >=0 && idade < 12){
+            img.setAttribute('src','menino.jpg')
+        }else if(idade < 21){
+            img.setAttribute('src','homemjovem.jpg')
+        }else if(idade < 50){
+            img.setAttribute('src','homem.jpg')
+        }else {
+            img.setAttribute('src','idoso.jpg')
+        }
+       } else if (fsex[1].checked){
+        genero = 'Mulher'
+         if(idade >=0 && idade < 12){
+            img.setAttribute('src','menina.jpg')
+        }else if(idade < 21){
+            img.setAttribute('src','mulherjovem.jpg')
+        }else if(idade < 50){
+            img.setAttribute('src','mulher.jpg')
+        }else {
+            img.setAttribute('src','idosa.jpg')
+        }
+       }
+       res.computedStyleMap.textAlign = 'center'
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+       res.appendChild(img)
+    }
 
+}
